44afe5ffaba118bab48d223987df21ad4d56227d
mugishab2020 <mugishab2020.com>
2023-05-24 15:08:28 -0700

    Betty is done

--- shell.h
+++ shell.h
@@ -28,13 +28,6 @@
 
 extern char **environ;
 
-
-/**
- * struct liststr - singly linked list
- * @num: the number field
- * @str: a string
- * @next: points to the next node
- */
 typedef struct liststr
 {
 	int numbers;
@@ -42,28 +35,6 @@ typedef struct liststr
 	struct liststr *next;
 } list_t;
 
-/**
- *struct pinfo - contains pseudo-arguements to pass into a function,
- *		allowing uniform prototype for function pointer struct
- *@arg: a string generated from getline containing arguements
- *@argv: an array of strings generated from arg
- *@path: a string path for the current command
- *@argc: the argument count
- *@line_count: the error count
- *@err_num: the error code for exit()s
- *@linecount_flag: if on count this line of input
- *@fname: the program filename
- *@env: linked list local copy of environ
- *@environ: custom modified copy of environ from LL env
- *@history: the history node
- *@alias: the alias node
- *@env_changed: on if environ was changed
- *@status: the return status of the last exec'd command
- *@cmd_buf: address of pointer to cmd_buf, on if chaining
- *@cmd_buf_type: CMD_type ||, &&, ;
- *@readfd: the fd from which to read line input
- *@histcount: the history line number count
- */
 typedef struct pinfo
 {
 	char *arv;
